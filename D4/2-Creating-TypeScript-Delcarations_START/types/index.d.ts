312ecfca8e828162b2f41dbef3aa79920bf5af95
Cody Nicholson <codynicholson96@gmail.com>
2022-06-28 21:01:52 -0500

    D4 done

---
+++ D4/2-Creating-TypeScript-Delcarations_START/types/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'lodash' {
+    function multiply(multiplier: number, multiplicand: number): number
+    function reverse(arr:string[]): string[]
+    function reverse(arr:number[]): number[]
+    function dropRight(arr:(string|number)[], dropped: number): (string|number)[]
+}
